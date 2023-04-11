eaf0f5e99e2757e97ccddfe2f28daafa98e81096
JoaoPrestupa <joao_prestupa11@hotmail.com>
2023-04-11 13:30:45 -0300

    backend teste v1

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,5 +1,5 @@
 # pylint: disable=E1101
-
+from typing import List
 from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
 from src.infra.entities import Users as UsersModel
@@ -8,7 +8,7 @@ from src.infra.entities import Users as UsersModel
 class UserRepository:
         """ Class to manage user repository """
 
-        
+            
 
         @classmethod  #cls == self / com classmethod        #um indicativo pra outros devs q sao strings
         def insert_user(cls, name: str, password: str) -> Users: # -> É OQ VAI RETORNAR
@@ -19,7 +19,7 @@ class UserRepository:
             :return tuple with new user
             """
 
-#para o erro do pylint sumir, tem q comentar na parte de cima do codigo ( pylint: disable=E1101 )
+    #para o erro do pylint sumir, tem q comentar na parte de cima do codigo ( pylint: disable=E1101 )
             with DBConnectionHandler() as db_connection:
                 try:
                     new_user = UsersModels(name=name, password=password)
@@ -33,6 +33,36 @@ class UserRepository:
                 finally:
                     db_connection.session.close()
             return none
+        
+        @classmethod
+        def select_user(cls, user_id: int = None, name: str = None) -> List[Users]:
+             """
+            Select data in user entity
+            :param - user_id: id of the registry
+                    -name: user name
+            return - List with Users selected
+             """
+                    
+        try:
+            query_data = None
+            #se tiver user id e nao name     
+            if user_id and not name:
+                with DBConnectionHandler() as db_connection: #Abre a conexao
+                    data = db_connection.session.query(UsersModel).filter_by(id=user_id).one() #joga em data a query(percorrendo).filtrar por ().devolva um
+                    query_data = [data]   #quando faz um select com query assim, é bom retornar com listas
+            elif not user_id and name:      
+                with DBConnectionHandler() as db_connetion:
+                    data = db_connection.session.query(UsersModel).filter_by(name=name).one()
+                    query_data = [data]
+            elif user_id and name:
+                with DBConnectionHandler() as db_connection: #pode ser qualquer var, só usada pra instanciar, a session ja e padrao do bd
+                    data = db_connection.session.query(UsersModel).filter_by(id=user_id, name=name).one()
+        except:
+            db_connection.session.rollback()           
+            raise
+        finally:
+            db_connection.close() 
+                    
 
 
 
